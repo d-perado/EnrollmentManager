47f49d778a6a44a1d01aad24e8d9c49c803cf593
d-perado <sjm01065@naver.com>
2026-04-22 17:57:46 +0900

    Initial commit

---
+++ src/main/java/org/example/enrollmentmanager/EnrollmentManagerApplication.java
@@ -0,0 +1,13 @@
+package org.example.enrollmentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrollmentManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnrollmentManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/enrollmentmanager/EnrollmentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.enrollmentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollmentManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
